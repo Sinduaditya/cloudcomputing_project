5290b05c78c5754fb39a743acea07fd414801f2a
Isa Fadhilah <isafadhilah05@gmail.com>
2025-05-16 07:25:34 +0000

    payment

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -224,17 +224,6 @@
   <div class="pc-container">
     <div class="pc-content">
       <!-- [ breadcrumb ] start -->
-      <div class="page-header">
-        <div class="page-block">
-          <div class="row align-items-center">
-            <div class="col-md-12">
-              <div class="page-header-title">
-                <h5 class="m-b-10">Home</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <!-- [ breadcrumb ] end -->
       <!-- [ Main Content ] start -->
       <div class="row">
